3d2c993ecd4697fa1844a3aed1212b9ea6f8cbcd
James Perrin <countrydawgg@gmail.com>
2023-01-27 08:30:36 -0800

    Complete Number and Boolean sections

---
+++ 02-javascript-fundamentals/exercises/boolean.js
@@ -0,0 +1,21 @@
+// The Boolean Data Type
+// NOT (!)
+// AND (&&)
+// OR  (||)
+
+console.log((!false && true) || !(false && true));  //true
+
+/**
+  Write 3 examples of expressions using each of the operations: !, &&, ||.
+  Print out the result of your expressions using console.log.
+  Be sure to test your work by running your code!
+*/
+
+// Example:
+console.log(false || false)   // false
+
+// Write 3 more expressions below:
+console.log(true && true);  // true
+console.log(false && true);  // false
+console.log(true || true);  // true
+console.log(false || true);  // true
