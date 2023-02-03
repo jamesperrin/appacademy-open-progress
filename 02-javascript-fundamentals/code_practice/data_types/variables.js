a628112d88ab0bd81b3b02be924b237ef03f3f46
James Perrin <countrydawgg@gmail.com>
2023-02-02 17:02:38 -0800

    Complete Data Type code practices

---
+++ 02-javascript-fundamentals/code_practice/data_types/variables.js
@@ -0,0 +1,35 @@
+let bootcamp = "App Academy";
+console.log(bootcamp); // 'App Academy'
+console.log('\n');
+
+let birthYear = 2012;
+console.log(birthYear); // 2012
+console.log('\n');
+
+// Variables
+// - initialization
+// - declartion
+// - assignment
+
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstName = 'King';
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+let lastName = 'Bob';
+
+// 3. Declare a variable called `age`:
+let age = 10;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(`firstName: ${firstName} \nlastName: ${lastName} \nage: ${age}`);
+console.log('\n');
+
+// 5. Assign the `age` variable to a number:
+age = 5;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(`firstName: ${firstName} \nlastName: ${lastName} \nage: ${age}`);
+console.log('\n');
