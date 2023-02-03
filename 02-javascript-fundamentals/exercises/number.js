b6a3efe48770911bca02eedbf826fd3fb6371758
James Perrin <countrydawgg@gmail.com>
2023-02-02 17:07:02 -0800

    Remove old files

--- 02-javascript-fundamentals/exercises/number.js
+++
@@ -1,8 +0,0 @@
-// Example:
-console.log(4 + 2.5)   // 6.5
-
-// Write 5 more expressions below:
-console.log(10 / 5); // 2
-console.log(10 * 5); // 50
-console.log(10 + 5); // 15
-console.log(10 - 5); // 5
