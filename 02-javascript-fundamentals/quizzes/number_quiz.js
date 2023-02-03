45064d581fbc59e2d75d167d521b3764dd875616
James Perrin <countrydawgg@gmail.com>
2023-02-02 17:05:29 -0800

    Complete JS Fundamental quizzes

---
+++ 02-javascript-fundamentals/quizzes/number_quiz.js
@@ -0,0 +1,11 @@
+console.log(9 + 4); // 13
+console.log(2 + 4 * 5); // 22
+console.log((2 + 4) * 5); // 30
+console.log(9 / 2); // 4.5
+console.log(25 % 7); // 4
+console.log(7 % 25); // 7
+console.log(3 % 12); // 3
+console.log(34 % 5); // 4
+console.log(30 % 5); // 0
+console.log(56 % 2); //
+console.log(99 % 2); // 1
